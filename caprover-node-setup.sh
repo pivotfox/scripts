b0fb9cb5f8ac6f4654f9f9446faa81beab29213f
Schickedanz <marc.schickedanz@pivotfox.de>
2019-05-02 11:36:08 +0200

    initial commit

---
+++ caprover-node-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#file should be run as root
+
+#Generate SWAP
+
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+#install Docker
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+
+#quick-fix: unable Firewall
+sudo ufw disable
