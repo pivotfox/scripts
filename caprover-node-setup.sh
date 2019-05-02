61d708c6c93ea2c4d393dc08d915efe34e357558
Schickedanz <marc.schickedanz@pivotfox.de>
2019-05-02 11:44:46 +0200

    added fetch of certificate

--- caprover-node-setup.sh
+++ caprover-node-setup.sh
@@ -12,7 +12,8 @@ cp /etc/fstab /etc/fstab.bak
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 
 #install Docker
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https ca-certificates software-properties-common
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 apt-cache policy docker-ce
